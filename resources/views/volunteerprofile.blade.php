8c350ae6c58f11de4c47a37653bcc95cab8b1350
rokib-ru-cse <rokibahmed.ru.cse@gmail.com>
2022-04-04 14:09:03 +0600

    fix color

--- resources/views/volunteerprofile.blade.php
+++ resources/views/volunteerprofile.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <div class="container-fluid pt-5"
-        style="background: linear-gradient(335deg, rgba(255,140,107,1) 0%, rgba(255,228,168,1) 100%);height:1000px">
+        style="background: linear-gradient(335deg, rgba(255,140,107,1) 0%, rgba(255,228,168,1) 100%);height:2000px">
         <div class="container">
             <h1 class="text-center mb-3">Your Profile</h1>
             <table class="table table-striped table-dark">
